58b8afaf948c5f4e55e84a79bfb8a688c9ca4950
sreerag <sreeragp2171@gmail.com>
2024-02-03 15:03:36 +0530

    images

--- src/App.js
+++ src/App.js
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
 
+import './App.css';
+import image1 from '../src/assets/image1.jpeg'
+import image2 from '../src/assets/image2.jpeg'
+import image3 from '../src/assets/image3.jpeg'
+import image4 from '../src/assets/image4.jpeg'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div className=" flex items-center justify-center">
+    
+      <div className=' flex justify-center items-center w-full gap-20 mt-20'>
+   
+        <div className='bg-yellow-200 w-2/12  mt-10'>
+ <img src={image1} alt="" className='h-80' />
+        </div>
+        <div className='bg-yellow-200 w-2/12  mt-10'>
+ <img src={image2} alt="" className='h-80' />
+        </div>
+        <div className='bg-yellow-200 w-2/12  mt-10'>
+ <img src={image3} alt="" className='h-80' />
+        </div>
+        <div className='bg-yellow-200 w-2/12  mt-10'>
+ <img src={image4} alt="" className='h-80' />
+        </div>
+      </div>
     </div>
   );
 }
